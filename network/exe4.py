18cae17d781a41b053bfdfbfa65ad8595206e5e5
hule2343 <aaa402262@gmail.com>
2023-10-27 16:25:05 +0900

    add beta_hyo

--- network/exe4.py
+++ network/exe4.py
@@ -5,8 +5,8 @@ from pytorch_lightning import Trainer
 import polars as pl
 
 def main():
-    train_data=pl.read_parquet("df\data9\\2016-2021-5-10.parquet")
-    test_data=pl.read_parquet("df\data9\\2022-5-1.parquet")
+    train_data=pl.read_parquet("df\data10\\2016-2021-5-10.parquet")
+    test_data=pl.read_parquet("df\data10\\2022-5-1.parquet")
     print(train_data.shape)
     print(test_data.shape)
     column_len=train_data.shape[1]-18
@@ -19,7 +19,7 @@ def main():
     if torch.cuda.is_available():
         trainer = Trainer(gpus=1, max_epochs=200)
     else:
-        trainer=Trainer(max_epochs=30)
+        trainer=Trainer(max_epochs=62)
 
     trainer.fit(horse_predictor, horse_data)
     trainer.test(horse_predictor, horse_data)
